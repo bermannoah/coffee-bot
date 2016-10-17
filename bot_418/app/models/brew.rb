b8f176a93ea45281815849f1aa05675b21abc40c
Noah Berman <noah@noahberman.org>
2016-10-16 21:34:36 -0600

    adds figaro

--- bot_418/app/models/brew.rb
+++ bot_418/app/models/brew.rb
@@ -2,7 +2,7 @@ class Brew < ApplicationRecord
   
   def self.create_new_brew(params)
     text = params["text"].split(' ')
-    location = text.shift
+    location = text.shift || "Blake"
     Brew.create!(
         user_name: params["user_name"],
         location: location,
