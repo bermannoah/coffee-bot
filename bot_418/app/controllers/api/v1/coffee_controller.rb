182c7f47884fd044712f41ed1c85f4a8b5b7b7c6
Noah Berman <noah@noahberman.org>
2016-10-16 19:12:52 -0600

    initial commit, rails setup, routes

---
+++ bot_418/app/controllers/api/v1/coffee_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::CoffeeController < Api::V1::BaseController
+  
+  def create
+    binding.pry
+  end
+  
+end
--- bot_418/config/routes.rb
+++ bot_418/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do 
+      post '/coffee_brewing', to: 'coffee#create'
+    end
+  end
+
 end
