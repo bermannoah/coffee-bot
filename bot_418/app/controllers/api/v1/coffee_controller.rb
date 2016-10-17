161d92bd2408ec8c40aed15e21ad4b807ac8d7fd
Noah Berman <noah@noahberman.org>
2016-10-16 19:29:45 -0600

    basic data parser complete

--- bot_418/app/controllers/api/v1/coffee_controller.rb
+++ bot_418/app/controllers/api/v1/coffee_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::CoffeeController < Api::V1::BaseController
   
   def create
     brew = Brew.create_new_brew(params)
+    binding.pry
   end
   
 end
--- bot_418/app/models/brew.rb
+++ bot_418/app/models/brew.rb
@@ -1,7 +1,13 @@
 class Brew < ApplicationRecord
   
   def self.create_new_brew(params)
-    
+    text = params["text"].split(' ')
+    location = text.shift
+    Brew.create!(
+        user_name: params["user_name"],
+        location: location,
+        description: text.join(' ')
+    )
     
     
   end
