d4bc549527b6bfd1093297d4d8a1967173a55857
Noah Berman <noah@noahberman.org>
2016-10-21 20:09:03 -0600

    basic view styling

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,7 +2,9 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-
+Rails.application.config.assets.precompile += %w( skeleton.css )
+Rails.application.config.assets.precompile += %w( normalize.css )
+Rails.application.config.assets.precompile += %w( application.css )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,8 @@ Rails.application.routes.draw do
   
   namespace :api do
     namespace :v1 do
-      get "/", to: "coffee#index"
+      root to: "coffee#index"
     end
   end
   
-
 end
