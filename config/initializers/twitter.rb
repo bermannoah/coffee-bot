fe2851dbc20d6216ac23292a957b7e9793f1caf7
Noah Berman <noah@noahberman.org>
2016-10-21 23:35:16 -0600

    adds twitter bot

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,7 +1,7 @@
 CLIENT = Twitter::REST::Client.new do |config| 
-  config.consumer_key = ENV['consumer_key'],
-  config.consumer_secret = ENV['consumer_secret'], 
-  config.access_token = ENV['access_token'], 
+  config.consumer_key = ENV['consumer_key']
+  config.consumer_secret = ENV['consumer_secret']
+  config.access_token = ENV['access_token']
   config.access_token_secret = ENV['access_secret'] 
 end
 
