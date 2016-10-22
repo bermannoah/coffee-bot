78229fe5b2f8726e2c504f39ea2b3d3893629347
Noah Berman <noah@noahberman.org>
2016-10-22 00:24:05 -0600

    quick fix

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -2,6 +2,6 @@ CLIENT = Twitter::REST::Client.new do |config|
   config.consumer_key = ENV['consumer_key']
   config.consumer_secret = ENV['consumer_secret']
   config.access_token = ENV['access_token']
-  config.access_token_secret = ENV['access_secret'] 
+  config.access_token_secret = ENV['access_token_secret'] 
 end
 
