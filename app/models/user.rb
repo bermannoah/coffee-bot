17ebee8831a82c2a7be5149bf4c7bba06d72085b
Noah Berman <noah@noahberman.org>
2016-11-15 11:37:43 -0700

    users can now have assigned subdomains

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
+  after_create :create_tenant
   has_secure_password
+  
+  private
+  
+  def create_tenant
+    Apartment::Tenant.create(subdomain)
+  end
+  
 end
