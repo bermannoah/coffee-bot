e3a3892b6d394faf424b5ff768f9c3f6f8de161d
Noah Berman <noah@noahberman.org>
2016-11-17 13:10:52 -0700

    add slack data to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161115192007) do
+ActiveRecord::Schema.define(version: 20161117200844) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,12 +24,14 @@ ActiveRecord::Schema.define(version: 20161115192007) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email"
     t.string   "password_digest"
     t.string   "subdomain"
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
-    t.integer  "role",            default: 0
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.integer  "role",               default: 0
+    t.string   "username"
+    t.string   "slack_uid"
+    t.text     "slack_access_token"
   end
 
 end
