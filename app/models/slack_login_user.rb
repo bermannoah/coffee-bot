2e552dcbc9d35734d40d4fdfe4c888116bb9898c
Noah Berman <noah@noahberman.org>
2016-11-18 09:34:01 -0700

    slack login users now associate with correct team

--- app/models/slack_login_user.rb
+++ app/models/slack_login_user.rb
@@ -10,10 +10,23 @@ class SlackLoginUser < ApplicationRecord
     user = find_or_initialize_by(slack_uid: user_info["user"]["id"]) do |u|
       u.username           = user_info["user"]["name"]
       u.slack_access_token = user_info["access_token"]
-      u.team               = user_info["team"]["id"]
+      u.team_id            = find_team(user_info)
     end
       user.save ? user : false
   end
+  
+  private
 
+  def self.find_team(user_info)
+    team = Team.find_by(team_slack_id: user_info["team"]["id"])
+    if team.nil?
+      team = Team.create(team_name: user_info["team"]["domain"], team_slack_id: user_info["team"]["id"])
+    else 
+      team = team
+    end
+    binding.pry
+    team.id
+  end
+    
   
 end
