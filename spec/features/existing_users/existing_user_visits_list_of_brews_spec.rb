e623e2b130d7f103fc4bd38406dca36b8fa79100
Noah Berman <noah@noahberman.org>
2017-01-02 13:36:26 -0700

    logged in user can see list of brews

--- spec/features/existing_users/existing_user_visits_list_of_brews_spec.rb
+++ spec/features/existing_users/existing_user_visits_list_of_brews_spec.rb
@@ -5,6 +5,7 @@ describe "Existing user visits list of brews page" do
     scenario "they see an empty page" do
       team = Fabricate(:team)
       user = Fabricate(:slack_login_user, team_id: team.id)
+      
       stub_logged_in_user(user)
         
       visit list_of_brews_path
@@ -22,6 +23,7 @@ describe "Existing user visits list of brews page" do
       team = Fabricate(:team)
       user = Fabricate(:slack_login_user, team_id: team.id)
       brew = Fabricate(:brew, team: team, user_name: user.username)
+      
       stub_logged_in_user(user)
         
       visit list_of_brews_path
@@ -32,6 +34,46 @@ describe "Existing user visits list of brews page" do
       expect(page).to have_content("Time Brewed")
       expect(page).to have_content("#{user.username}")
       expect(page).to have_content("#{brew.location}")
+    end
+
+    scenario "they do not see other company's brews" do
+      team = Fabricate(:team)
+      user = Fabricate(:slack_login_user, team_id: team.id)
+      brew_1 = Fabricate(:brew, team: team, user_name: user.username)
+      brew_2 = Fabricate(:brew)
+      
+      stub_logged_in_user(user)
+        
+      visit list_of_brews_path
+        
+      expect(page).to have_content("#{user.username}")
+      expect(page).to have_content("#{brew_1.location}")
+      expect(page).to_not have_content("#{brew_2.location}")
+    end
+
+    scenario "they see multiple user's brews" do
+      team = Fabricate(:team)
+      
+      user_1 = Fabricate(:slack_login_user, team_id: team.id)
+      user_2 = Fabricate(:slack_login_user, team_id: team.id)
+      user_3 = Fabricate(:slack_login_user, team_id: team.id)
+      
+      brew_1 = Fabricate(:brew, team: team, user_name: user_1.username)
+      brew_2 = Fabricate(:brew, team: team, user_name: user_2.username)
+      brew_3 = Fabricate(:brew, team: team, user_name: user_3.username)
+      brew_4 = Fabricate(:brew, team: team, user_name: user_1.username)
+      brew_5 = Fabricate(:brew, team: team, user_name: user_1.username)
+      brew_6 = Fabricate(:brew, team: team, user_name: user_3.username)
+      
+      stub_logged_in_user(user_1)
+        
+      visit list_of_brews_path
+        
+      expect(Brew.count).to eq(6)
+      expect(page).to have_content("#{user_1.username}")
+      expect(page).to have_content("#{brew_1.user_name}")
+      expect(page).to have_content("#{brew_2.user_name}")
+      expect(page).to have_content("#{brew_3.user_name}")
     end
   end
 end
