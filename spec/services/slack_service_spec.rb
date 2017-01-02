b441fad7888de93d7f9e714fa8b145ecacbe239e
Noah Berman <noah@noahberman.org>
2017-01-02 12:13:37 -0700

    begin tests for slack service

---
+++ spec/services/slack_service_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe SlackService, type: :service do
+  context "returns error from Slack validation" do
+    scenario "#authenticate returns false" do
+      params = {"error" => "access_denied",
+                "state" => ""}
+                
+      expect(SlackService.authenticate(params)).to eq(false)
+    end
+  end
+end
