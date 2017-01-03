9071223c1ea7c91445e297f2ac4c7a9497c4c9ce
Noah Berman <noah@noahberman.org>
2017-01-03 14:02:09 -0700

    happy and sad paths for /coffee_brewing options

--- spec/requests/api/v1/coffee_endpoint_spec.rb
+++ spec/requests/api/v1/coffee_endpoint_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe "coffee_brewing endpoint" do
+describe "coffee_brewing endpoint POST requests" do
   context 'POST /api/v1/coffee_brewing' do
     it 'creates a new brew' do
       team = Fabricate(:team)
@@ -68,7 +68,85 @@ describe "coffee_brewing endpoint" do
       expect(Brew.first.location).to eq(team.team_name)
       expect(brew["text"]).to eq("Hey #{user.username} thanks for brewing coffee! You're a hero!")
     end
+  
+    it 'returns a how-to guide with #how' do
+      team = Fabricate(:team)
+      user = Fabricate(:slack_login_user, team_id: team.id)
+      data = { team_id: team.id, 
+               team_domain: team.team_name,
+               user_id: user.id,
+               user_name: user.username,
+               command: "/coffee_brewing",
+               text: "#how"
+             }
+      
+      post "/api/v1/coffee_brewing", params: data.to_json, headers: { 'CONTENT_TYPE' => 'application/json'}
+      
+      result = JSON.parse(response.body)
+
+      expect(result["text"]).to eq("Hey #{user.username}  thanks for asking!")
+      expect(result["attachments"][0]["text"]).to include("These are pretty general instructions for brewing coffee")
+    end
+
+    it 'returns out of coffee instructions with #out' do
+      team = Fabricate(:team)
+      user = Fabricate(:slack_login_user, team_id: team.id)
+      data = { team_id: team.id, 
+               team_domain: team.team_name,
+               user_id: user.id,
+               user_name: user.username,
+               command: "/coffee_brewing",
+               text: "#out"
+             }
+      
+      post "/api/v1/coffee_brewing", params: data.to_json, headers: { 'CONTENT_TYPE' => 'application/json'}
+      
+      result = JSON.parse(response.body)
+
+      expect(result["text"]).to eq("Hey #{user.username} thanks for being so conscientious!")
+      expect(result["attachments"][0]["text"]).to include("...but this feature is still in development.")
+    end
     
+    it 'does not brew a new coffee with #how' do
+      team = Fabricate(:team)
+      user = Fabricate(:slack_login_user, team_id: team.id)
+      data = { team_id: team.id, 
+               team_domain: team.team_name,
+               user_id: user.id,
+               user_name: user.username,
+               command: "/coffee_brewing",
+               text: "#how"
+             }
+      expect(Brew.count).to eq(0)
+      
+      post "/api/v1/coffee_brewing", params: data.to_json, headers: { 'CONTENT_TYPE' => 'application/json'}
+      
+      result = JSON.parse(response.body)
+
+      expect(Brew.count).to eq(0)
+      expect(result["text"]).to eq("Hey #{user.username}  thanks for asking!")
+    end
+    
+    it 'does not brew a new coffee with #out' do
+      team = Fabricate(:team)
+      user = Fabricate(:slack_login_user, team_id: team.id)
+      data = { team_id: team.id, 
+               team_domain: team.team_name,
+               user_id: user.id,
+               user_name: user.username,
+               command: "/coffee_brewing",
+               text: "#out"
+             }
+      expect(Brew.count).to eq(0)
+      
+      post "/api/v1/coffee_brewing", params: data.to_json, headers: { 'CONTENT_TYPE' => 'application/json'}
+      
+      result = JSON.parse(response.body)
+
+      expect(Brew.count).to eq(0)
+      expect(result["text"]).to eq("Hey #{user.username} thanks for being so conscientious!")
+    end
+        
     it 'does not create a new brew without a team' do
       team = Fabricate(:team)
       user = Fabricate(:slack_login_user, team_id: team.id)
