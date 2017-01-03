e1afc82510eb3b8732957a02a3092b0bd2177a50
Noah Berman <noah@noahberman.org>
2017-01-03 13:35:16 -0700

    coffee post test

--- spec/requests/api/v1/coffee_endpoint_spec.rb
+++ spec/requests/api/v1/coffee_endpoint_spec.rb
@@ -5,12 +5,20 @@ describe "coffee endpoint" do
     it 'creates a new coffee' do
       team = Fabricate(:team)
       user = Fabricate(:slack_login_user, team_id: team.id)
-
-      post "/api/v1/coffee_brewing"
+      data = { team_id: team.id, 
+               team_domain: team.team_name,
+               user_id: user.id,
+               user_name: user.username,
+               command: "/coffee_brewing",
+               text: ""
+             }
+      expect(Brew.count).to eq(0)
+      post "/api/v1/coffee_brewing", params: data.to_json, headers: { 'CONTENT_TYPE' => 'application/json'}
       
       brew = JSON.parse(response.body)
-      binding.pry
+
       expect(response).to be_success
+      expect(Brew.count).to eq(1)
     end
   end
 end
