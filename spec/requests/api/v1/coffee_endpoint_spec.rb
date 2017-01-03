a37f6a80542b45e1c07b9a3e29479c92b6af3acd
Noah Berman <noah@noahberman.org>
2017-01-03 13:27:27 -0700

    fix strange team import error

--- spec/requests/api/v1/coffee_endpoint_spec.rb
+++ spec/requests/api/v1/coffee_endpoint_spec.rb
@@ -2,10 +2,15 @@ require 'rails_helper'
 
 describe "coffee endpoint" do
   context 'POST /api/v1/coffee_brewing' do
-    xit 'creates a new coffee' do
+    it 'creates a new coffee' do
       team = Fabricate(:team)
       user = Fabricate(:slack_login_user, team_id: team.id)
 
+      post "/api/v1/coffee_brewing"
+      
+      brew = JSON.parse(response.body)
+      binding.pry
+      expect(response).to be_success
     end
   end
 end
