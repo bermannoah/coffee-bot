32920dc07fd0dccbdb5790795a3a7aad6e25efaf
Noah Berman <noah@noahberman.org>
2017-01-03 14:26:11 -0700

    test for multiple /last_brewed responses

--- spec/requests/api/v1/coffee/get_to_coffee_brewing_endpoint_spec.rb
+++ spec/requests/api/v1/coffee/get_to_coffee_brewing_endpoint_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "coffee_brewing endpoint GET requests" do
   context 'GET /api/v1/coffee_brewing' do
-    it 'returns recent brew with /last_brewed' do
+    it 'returns most recent brew with /last_brewed' do
       team = Fabricate(:team, team_slack_id: "1234")
       user = Fabricate(:slack_login_user, team_id: team.id)
       brew = Fabricate(:brew, team: team, user_name: user.username, location: "Home", description: "delicious!")
@@ -24,6 +24,35 @@ describe "coffee_brewing endpoint GET requests" do
       expect(brew["text"]).to eq("Last coffee brew(s):")
       expect(brew["attachments"][0]["text"]).to include("Coffee was brewed in Home at")
       expect(brew["attachments"][0]["text"]).to include("delicious!")
+    end
+    
+    it 'returns multiple brews with /last_brewed if given a number' do
+      team = Fabricate(:team, team_slack_id: "1234")
+      user = Fabricate(:slack_login_user, team_id: team.id)
+      brew_1 = Fabricate(:brew, team: team, user_name: user.username, location: "Home", description: "delicious!")
+      brew_2 = Fabricate(:brew, team: team, user_name: user.username, location: "Turing", description: "so good!")
+      brew_3 = Fabricate(:brew, team: team, user_name: user.username, location: "The Moon", description: "made me feel weightless!")
+      brew_4 = Fabricate(:brew, team: team, user_name: user.username, location: "Home", description: "best!")
+      data = { 
+               team_id: "1234", 
+               team_domain: team.team_name,
+               user_id: user.id,
+               user_name: user.username,
+               command: "/last_brewed",
+               text: "4"
+             }
+      expect(Brew.count).to eq(4)
+
+      get "/api/v1/coffee_brewing", params: data.stringify_keys, headers: { 'CONTENT_TYPE' => 'application/json'}
+      
+      brews = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(brews["text"]).to eq("Last coffee brew(s):")
+      expect(brews["attachments"][0]["text"]).to include("Coffee was brewed in The Moon at")
+      expect(brews["attachments"][0]["text"]).to include("made me feel weightless!")
+      expect(brews["attachments"][0]["text"]).to include("delicious!")
+      expect(brews["attachments"][0]["text"]).to include("Turing")
     end
   end
 end
