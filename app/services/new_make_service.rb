469dd897094431e144c309ea27472e4d726747c1
Noah Berman <noah@noahberman.org>
2017-01-04 17:58:06 -0700

    happy path testing for new make service

--- app/services/new_make_service.rb
+++ app/services/new_make_service.rb
@@ -1,7 +1,21 @@
 module NewMakeService
   
   def self.start_making_a_drink(type)
-    
+    send_make_request(type)
+  end
+  
+  private
+  
+  def self.conn
+    Faraday.new(:url => "http://localhost:8080") do |faraday|
+      faraday.request :url_encoded
+      faraday.adapter Faraday.default_adapter
+    end
+  end
+  
+  def self.send_make_request(type)
+    response = conn.post '/api/v1/makes', { :type => type }
+    JSON.parse(response.body)
   end
   
 end
--- spec/services/make_service_spec.rb
+++ spec/services/make_service_spec.rb
@@ -4,11 +4,21 @@ describe NewMakeService, type: :service do
   context "with a server on localhost" do
     scenario "it POSTs a new make", :vcr do
       make_type = "coffee"
-      maker_bot_response = NewMakeService.start_making_a_drink(type = make_type)
+      maker_bot_response = NewMakeService.start_making_a_drink(make_type)
 
       expect(maker_bot_response).to be_a(Hash)
       expect(maker_bot_response).to have_key("message")
-      expect(posted_repo_response).to have_value("Heating water for coffee!")
+      expect(maker_bot_response).to have_value("Heating water for coffee!")
+    end
+    
+    scenario "it gets the right response for different kinds of drinks", :vcr do
+      maker_bot_response_1 = NewMakeService.start_making_a_drink("coffee")
+      maker_bot_response_2 = NewMakeService.start_making_a_drink("tea")
+      maker_bot_response_3 = NewMakeService.start_making_a_drink("water")
+
+      expect(maker_bot_response_1).to have_value("Heating water for coffee!")
+      expect(maker_bot_response_2).to have_value("Heating water for tea!")
+      expect(maker_bot_response_3).to have_value("Heating water!")
     end
   end
 end
