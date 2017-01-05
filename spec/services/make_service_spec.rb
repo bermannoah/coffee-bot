600d05be8f4d3583f14dd67dd0766f50d084f212
Noah Berman <noah@noahberman.org>
2017-01-04 18:01:28 -0700

    happy and sad paths for current new make service

--- spec/services/make_service_spec.rb
+++ spec/services/make_service_spec.rb
@@ -20,5 +20,21 @@ describe NewMakeService, type: :service do
       expect(maker_bot_response_2).to have_value("Heating water for tea!")
       expect(maker_bot_response_3).to have_value("Heating water!")
     end
+
+    scenario "it returns an error for an improper request", :vcr do
+      maker_bot_response_1 = NewMakeService.start_making_a_drink("balloons")
+      maker_bot_response_2 = NewMakeService.start_making_a_drink(1234)
+      maker_bot_response_3 = NewMakeService.start_making_a_drink("Robert'); DROP TABLE Makes;--")
+
+      expect(maker_bot_response_1).to have_value("Not sure what you meant by that.")
+      expect(maker_bot_response_2).to have_value("Not sure what you meant by that.")
+      expect(maker_bot_response_3).to have_value("Not sure what you meant by that.")
+    end
+
+    scenario "it returns an error if not given anything to brew", :vcr do
+      maker_bot_response_1 = NewMakeService.start_making_a_drink("")
+
+      expect(maker_bot_response_1).to have_value("Not sure what you meant by that.")
+    end
   end
 end
