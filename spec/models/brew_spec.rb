5401c70c40aecc959960b5a0d72a15895a1ad8cb
Noah Berman <noah@noahberman.org>
2017-01-11 20:02:21 -0700

    minor cosmetic refactor and codeclimate additions

--- spec/models/brew_spec.rb
+++ spec/models/brew_spec.rb
@@ -4,4 +4,11 @@ describe Brew, type: :model do
   context 'relationships' do
     it { should belong_to(:team) }
   end
+  
+  context 'methods' do
+    it 'returns false for retrieve recent brew if there is no current user' do
+      current_user = nil
+      expect(Brew.retrieve_recent_brew(current_user)).to be false
+    end
+  end
 end
