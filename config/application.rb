a260ed71513af773a983589cda2b759c6c6f5d81
Noah Berman <noah@noahberman.org>
2017-05-30 22:15:18 +0200

    hotfix for skylight removal

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,5 @@ module CoffeeBot
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.active_record.default_timezone = :local
-    config.skylight.environments += ['development']
   end
 end
