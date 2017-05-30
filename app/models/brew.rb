bf220e2523d645dc0af3801da2d4b5154f1db3dc
Noah Berman <noah@noahberman.org>
2017-05-30 22:40:58 +0200

    copy adjustment for index page

--- app/models/brew.rb
+++ app/models/brew.rb
@@ -121,7 +121,7 @@ class Brew < ApplicationRecord
   
   def self.index_brew_display(current_user)
     if current_user && retrieve_recent_brew(current_user)
-      return "Most recent coffee brew: #{@recent_brew.location} #{ApplicationController.helpers.time_ago_in_words(@recent_brew.created_at)}."
+      return "Welcome! Coffee was brewed in #{@recent_brew.location} #{ApplicationController.helpers.time_ago_in_words(@recent_brew.created_at)} ago."
     elsif current_user && !retrieve_recent_brew(current_user)
       return "No coffee has been brewed yet. Use the commands below to start logging brews!"
     end
