c115648bae20b8d75a6e9bc06294ee493bcacb03
Noah Berman <noah@noahberman.org>
2017-06-20 23:05:27 +0200

    Correct syntax error

--- app/services/webhook_service.rb
+++ app/services/webhook_service.rb
@@ -2,7 +2,7 @@ module WebhookService
   
   def coffee_is_brewing(time,params,webhook_url)
     conn = Faraday.new(:url => webhook_url)
-    conn.post {"text":"Coffee started brewing at #{time}!"}
+    conn.post {text:"Coffee started brewing at #{time}!"}
   end
   
 end
