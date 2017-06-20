c2ee9386dba6af186f0e6acc31ae5104943550c2
Noah Berman <noah@noahberman.org>
2017-06-20 23:19:11 +0200

    Make faraday req more explicit

--- app/services/webhook_service.rb
+++ app/services/webhook_service.rb
@@ -2,7 +2,12 @@ module WebhookService
   
   def coffee_is_brewing(time,params,webhook_url)
     conn = Faraday.new(:url => webhook_url)
-    conn.post {text:"Coffee started brewing at #{time}!"}
+    body_text = {text:'Coffee started brewing at #{time}!'}
+    conn.post do |req|
+      req.url webhook_url
+      req.headers['Content-Type' = 'application/json']
+      req.body = body_text.to_json
+    end
   end
   
 end
