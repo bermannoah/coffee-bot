7a7c3237a16ce1d534a1ee09ba94cba789cb4f24
Noah Berman <noah@noahberman.org>
2017-06-25 16:28:27 +0200

    Simplify

--- app/services/webhook_service.rb
+++ app/services/webhook_service.rb
@@ -3,36 +3,15 @@ module WebhookService
   def self.coffee_is_brewing(webhook_url,params)
     conn = Faraday.new(:url => webhook_url)
     if params["description"] != "" 
-      body_text = message_to_send_with_comment(params).to_s
+      body_text = {text:"#{params["user_name"]} has just started brewing coffee in #{params["location"]}!",
+                   attachments:"#{params["description"]}"}
     else 
-      body_text = {text:"#{user_name} has just started brewing coffee!"}
+      body_text = {text:"#{params["user_name"]} has just started brewing coffee in #{params["location"]}!"}
     end
     conn.post do |req|
       req.url webhook_url
       req.headers['Content-Type'] = 'application/json'
       req.body = body_text
     end
-  end
-  
-  def self.message_to_send_with_comment(params)
-    {
-    "attachments": [
-        {
-            "fallback": "#{params["user_name"]} has started brewing coffee!",
-            "pretext": "#{params["user_name"]} has started brewing coffee!",
-            "text": "#{params["description"]}",
-            "fields": [
-                  {
-                      "title": "Location",
-                      "value": "#{params["location"]}",
-                      "short": false
-                  }
-                ],
-              "footer": "CoffeeBot",
-              "footer_icon": "☕️"
-          }
-      ]
-    }
-  end
-  
+  end  
 end
