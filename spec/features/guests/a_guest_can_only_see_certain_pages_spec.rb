22b4fbb95a6cf6258462e72b163982d70ebe76f0
Noah Berman <noah@noahberman.org>
2018-12-31 23:23:53 +0100

    Modify privacy policy w US server and add back buttons

--- spec/features/guests/a_guest_can_only_see_certain_pages_spec.rb
+++ spec/features/guests/a_guest_can_only_see_certain_pages_spec.rb
@@ -9,21 +9,24 @@ describe 'A guest user visits the site' do
       expect(page).to have_content('/kettle_brewing')
       expect(page).to have_content('/last_brewed')
       expect(page).to have_content('GET')
-      expect(page).to have_content('coffeebot!')
+      expect(page).to have_content('coffeebot')
     end
 
     scenario 'they visit the privacy page' do
       visit privacy_path
 
-      expect(page).to have_content('coffeebot!')
+      expect(page).to have_content('coffeebot')
       expect(page).to have_content('Privacy Policy')
+      expect(page).to have_content('hello at coffeebot.coffee')
+      expect(page).to have_link('back')
     end
 
     scenario 'they visit the cookie policy page' do
       visit cookies_path
 
-      expect(page).to have_content('coffeebot!')
+      expect(page).to have_content('coffeebot')
       expect(page).to have_content('If we update this policy this page will update.')
+      expect(page).to have_link('back')
     end
   end
 end
