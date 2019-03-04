33809d5754b3928d286c0adbeb03e1531cbfc7f7
Noah Berman <noah@noahberman.org>
2019-03-04 22:55:58 +0100

    Test fixup and span to p

--- spec/features/guests/a_guest_can_only_see_certain_pages_spec.rb
+++ spec/features/guests/a_guest_can_only_see_certain_pages_spec.rb
@@ -6,9 +6,8 @@ describe 'A guest user visits the site' do
       visit root_path
 
       expect(page).to have_content('/coffee_brewing')
-      expect(page).to have_content('/kettle_brewing')
       expect(page).to have_content('/last_brewed')
-      expect(page).to have_content('GET')
+      expect(page).to have_content('POST')
       expect(page).to have_content('coffeebot')
     end
 
